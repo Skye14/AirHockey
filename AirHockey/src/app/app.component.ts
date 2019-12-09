6afcc0d46360e810486909b48fa6c3ee850a1c1c
Skye <skyekitty@live.com>
2019-12-09 13:04:19 +0300

    Delete empty files.

--- AirHockey/src/app/app.component.ts
+++ AirHockey/src/app/app.component.ts
@@ -5,8 +5,7 @@ import { AuthService } from './shared/services/auth.service';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
     constructor(private translate: TranslateService, private authService: AuthService) {
--- AirHockey/src/app/game/game.component.ts
+++ AirHockey/src/app/game/game.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+    selector: 'app-game',
+    templateUrl: './game.component.html'
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
